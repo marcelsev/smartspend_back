04e1b7f9019a72066013698eea87c88c4c82beee
marcelo.solano <marcelosolanosev@gmail.com>
2024-04-12 18:49:36 +0200

    profil expense deposit

--- controllers/expensePostController.js
+++ controllers/expensePostController.js
@@ -33,7 +33,20 @@ module.exports = {
             res.status(500).send('Error server');
         }
     },
-    
+    async getExpensesByUserId(req, res) {
+        try {
+            const { userId } = req.params;
+
+            const expenses = await Expense.findAll({
+                where: { UserId: userId }
+            });
+
+            res.json(expenses);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal server error');
+        }
+    },
     async  getExpenseById(req, res) {
       try {
           const { id } = req.params;
@@ -99,3 +112,62 @@ module.exports = {
   
 }
 
+/* async createExpense(req, res) {
+        try {
+            const { amount, exp_date, location, note, categoryId, userId, methodPayId } = req.body;
+            const category = await Category.findByPk(categoryId);
+            const user = await User.findByPk(userId);
+            const method_pay = await Method_pay.findByPk(methodPayId);
+
+            if (!category || !user || !method_pay) {
+                return res.status(404).json({ error: 'Data not found' });
+            }
+
+            const newExpense = await Expense.create({
+                amount,
+                exp_date,
+                location,
+                note,
+                CategoryId: category.id,
+                UserId: user.id,
+                MethodPayId: method_pay.id
+            });
+
+            res.json(newExpense);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal server error');
+        }
+    },
+
+    async getExpensesByUserId(req, res) {
+        try {
+            const { userId } = req.params;
+
+            const expenses = await Expense.findAll({
+                where: { UserId: userId }
+            });
+
+            res.json(expenses);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal server error');
+        }
+    },
+
+    async getExpenseById(req, res) {
+        try {
+            const { id } = req.params;
+
+            const expense = await Expense.findByPk(id);
+
+            if (!expense) {
+                return res.status(404).json({ error: 'Expense not found' });
+            }
+
+            res.json(expense);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal server error');
+        }
+    }, */
--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const ExpenseController = require('../controllers/expensePostController');
-
+const {authenticate} = require('../controllers/middleware/authMiddleware');
 // CRUD routes
 router.post('/expense', ExpenseController.createExpense);
 router.get('/expense', ExpenseController.getExpenses);
 router.get('/expense/:id', ExpenseController.getExpenseById);
+router.get('/expense/:id',authenticate, ExpenseController.getExpensesByUserId);
 router.put('/expense/:id', ExpenseController.updateExpense);
 router.delete('/expense/:id', ExpenseController.deleteExpense);
 module.exports = router;
