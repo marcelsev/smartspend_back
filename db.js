f88e56c5ae9903fde7cbd48b95a1cf963d5b0854
marcelo.solano <marcelosolanosev@gmail.com>
2024-05-31 21:15:30 +0200

    regler le railway

--- db.js
+++ db.js
@@ -16,12 +16,18 @@ const env = process.env.NODE_ENV || 'development';
 let sequelize;
 
 if (env === 'test') {
-  sequelize = new Sequelize({dialect: 'sqlite',
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
     storage: 'sequelize.sqlite',
   });
 } else {
-  sequelize = new Sequelize('smartspend_db', 'root', '', {
-    host: 'localhost',
+  const dbName = process.env.DB_NAME || 'smartspend_db';
+  const dbUser = process.env.DB_USER || 'root';
+  const dbPass = process.env.DB_PASS || '';
+  const dbHost = process.env.DB_HOST || 'localhost';
+
+  sequelize = new Sequelize(dbName, dbUser, dbPass, {
+    host: dbHost,
     dialect: 'mysql',
   });
 }
